0232225e312c986252b9bbaba549324e5c6ff9f0
Akhmatamin <ahmatamin.raimzhanov@alatoo.edu.kg>
2025-02-21 16:37:08 +0600

    Create question.js
    script questions

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "D. Every one knows it",
+        options: [
+            "A. I dont know",
+            "B. You know it",
+            "C. Who knows it ?",
+            "D. Every one knows it",
+        ]
+    },
+    {
+        numb: 2,
+        question: "who are you?",
+        answer: "person",
+        options: [
+            "person",
+            "animal",
+            "sheet",
+            "cat",
+        ]
+    },
+    {
+        numb: 3,
+        question: "where are you from?",
+        answer: "bishkek",
+        options: [
+            "London",
+            "parij",
+            "tokyo",
+            "bishkek",
+        ]
+    },
+    {
+        numb: 4,
+        question: "what is your name?",
+        answer: "yo",
+        options: [
+            "nnljchldj",
+            "fjldf",
+            "yo",
+            "fdfkjl",
+        ]
+    },
+    {
+        numb: 5,
+        question: "what is it?",
+        answer: "book",
+        options: [
+            "book",
+            "phone",
+            "man",
+            "bear",
+        ]
+    }
+];
